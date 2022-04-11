af7b28646d4b78ecd411180f7ec6bd9dda1b07b8
Calypso Leonard <calypsorleonard@gmail.com>
2022-04-11 16:13:33 -0400

    all deliverables met

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,20 @@
 class Dev < ActiveRecord::Base
+    has_many :freebies
+    has_many :devs, through: :freebies
 
+    def recieved_one? item_name
+        has = false
+        self.freebies.each do |freebie|
+            if freebie.item_name == item_name
+                has = true
+            end
+        end
+        has
+    end
+
+    def give_away dev, freebie
+        if freebie.dev == self
+            freebie.update(dev: dev)
+        end
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,23 @@
 puts "Creating companies..."
-Company.create(name: "Google", founding_year: 1998)
-Company.create(name: "Facebook", founding_year: 2004)
-Company.create(name: "Dunder Mifflin", founding_year: 2002)
-Company.create(name: "Enron", founding_year: 1995)
+comp1= Company.create(name: "Google", founding_year: 1998)
+comp2 = Company.create(name: "Facebook", founding_year: 2004)
+comp3 = Company.create(name: "Dunder Mifflin", founding_year: 2002)
+comp4 = Company.create(name: "Enron", founding_year: 1995)
 
 puts "Creating devs..."
-Dev.create(name: "Rick")
-Dev.create(name: "Morty")
-Dev.create(name: "Mr. Meseeks")
-Dev.create(name: "Gazorpazop")
+dev1 = Dev.create(name: "Rick")
+dev2 = Dev.create(name: "Morty")
+dev3 = Dev.create(name: "Mr. Meseeks")
+dev4 = Dev.create(name: "Gazorpazop")
 
 puts "Creating freebies..."
+
+
+Freebie.create(item_name: "bob", value: 5, dev: dev1, company: comp4)
+Freebie.create(item_name: "joe", value: 1, dev: dev2, company: comp3)
+Freebie.create(item_name: "sal", value: 2, dev: dev3, company: comp2)
+Freebie.create(item_name: "vish", value: 6, dev: dev4, company: comp1)
+
 
 # ***************************************************************
 # * TODO: create freebies! Remember, a freebie belongs to a dev *
